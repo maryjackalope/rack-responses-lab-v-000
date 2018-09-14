7c064d70672e6d429b5717c18f1bb2202d6213aa
Mary Jack <maryjackinthebox@gmail.com>
2018-09-14 18:45:57 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,8 +3,8 @@ class Application
   def call(env)
     resp = Rack::Response.new
  
-   
-    if Time 
+    t = Time.now
+    if t <12 
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
