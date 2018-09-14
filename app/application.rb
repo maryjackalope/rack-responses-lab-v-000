36d9c556a695f3bcd50404df2a841baa4573024c
Mary Jack <maryjackinthebox@gmail.com>
2018-09-14 18:40:22 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+class Application
+ 
+  def call(env)
+    resp = Rack::Response.new
+ 
+   
+    if num_1==num_2 && num_2==num_3
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+ 
+    resp.finish
+  end
+ 
+end
